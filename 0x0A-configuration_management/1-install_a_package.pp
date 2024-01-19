12d6a5b6a826c9a592cac4596a6367e4e84bc616
Love <mzeelovegeneral@gmail.com>
2024-01-19 22:21:09 +0100

    install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 #install flast on agent sever
-exec { 'install_flask':
-  command => 'usr/bin/pip3 install Flask==2.1.0',
-  path    => ['usr/bin/pip3', 'usr/local/bin/pip3'],
-  unless  => 'usr/bin/pip3' show Flask | grep -q 'version: 2.1.0',
+exec { 'Flask':
+  ensure   => '2.1.0'
+  provider => pip3,
 }
