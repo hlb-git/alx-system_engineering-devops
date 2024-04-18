16ce8da51a658f2812e5c49baf04f5629e86d307
Love <mzeelovegeneral@gmail.com>
2024-04-18 22:50:13 +0100

    feat(puppet): change user permission for hoberton

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,6 @@
+# make hoberton user open files without error
+
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+}
